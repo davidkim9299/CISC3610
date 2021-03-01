869aaf901ef2b7f1c9a39b4539a984b21692f9d4
davidkim9299 <davidkim9299@gmail.com>
2021-03-01 18:21:12 -0500

    edit text

--- docs/CHART/chart.js
+++ docs/CHART/chart.js
@@ -1,17 +1,13 @@
 const canvas = document.getElementById('canvas_chart');
 const chart = canvas.getContext('2d');
 
-//Array of Fruits
-var fruits = {
-	"name":["Apple","Orange", "Banana", "Kiwi", "Blueberry", "Grapes"],
-	"quantity":[20,10,15,3,5,8],
-	"color":['red','orange','yellow','green','skyblue','purple']
-};
 
 function drawchart(x, y, length, width, color){
 	chart.fillStyle = color;
   	chart.fillRect(x, y, length, width);
 }
+
+
 
 function filltext(text, font, color, x, y){
   	chart.font = font;
@@ -20,6 +16,13 @@ function filltext(text, font, color, x, y){
   	chart.fillText(text, x, y);
   
 } 
+
+//Array of Fruits
+var fruits = {
+	"name":["Apple","Orange", "Banana", "Kiwi", "Blueberry", "Grapes"],
+	"quantity":[20,10,15,3,5,8],
+	"color":['red','orange','yellow','green','skyblue','purple']
+};
 
 function findchartfit(){
 	var fit = 0;
@@ -33,10 +36,12 @@ function findchartfit(){
  	}
 }
 
+
+
 var fitnum = findchartfit();
 
 for(var f = 0; f < canvas.height/100; f++) {
 	drawchart(f*100, 500, 100, (fruits.quantity[f])*fitnum, fruits.color[f]);
-	filltext(fruits.quantity[f],'bold 20px arial','black',((f*100) + 50),550);
-	filltext(fruits.name[f],'bold 18px arial','black',(f*100)+50,575);
+	filltext(fruits.quantity[f],'bold 20px arial','black',((f*100) + 50),460);
+	filltext(fruits.name[f],'bold 20px arial','black',(f*100)+50,480);
 }
