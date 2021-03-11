6935585d8affa8bfc019c4234ba6ea20c690ae71
davidkim9299 <davidkim9299@gmail.com>
2021-03-11 03:05:43 -0500

    Create Skeletonjson.js

---
+++ docs/ANIMATION/Skeletonjson.js
@@ -0,0 +1,372 @@
+Skeleton={
+	src : "spritesheet.png",
+	img: undefined,
+	frame:0,
+	offset: 0,
+	scale: 0.5,
+	"frames": [
+		{
+			"filename": "tile000.png",
+			"frame": {
+				"x": 17,
+				"y": 19,
+				"w": 107,
+				"h": 173
+			},
+			"sourceSize": {
+				"w": 107,
+				"h": 173
+			}
+		},
+
+
+		{
+			"filename": "tile001.png",
+			"frame": {
+				"x": 188,
+				"y": 28,
+				"w": 77,
+				"h": 164
+			},
+			"sourceSize": {
+				"w": 77,
+				"h": 164
+			}
+		},
+
+
+		{
+			"filename": "tile002.png",
+			"frame": {
+				"x": 346,
+				"y": 24,
+				"w": 74,
+				"h": 168
+			},
+			"sourceSize": {
+				"w": 74,
+				"h": 168
+			}
+		},
+
+
+		{
+			"filename": "tile003.png",
+			"frame": {
+				"x": 489,
+				"y": 16,
+				"w": 91,
+				"h": 176
+			},
+			"sourceSize": {
+				"w": 91,
+				"h": 176
+			}
+		},
+
+
+		{	
+			"filename": "tile004.png",
+			"frame": {
+				"x": 631,
+				"y": 9,
+				"w": 117,
+				"h": 184
+			},
+			"sourceSize": {
+				"w": 117,
+				"h": 184
+			}
+		},
+
+
+		{
+			"filename": "tile005.png",
+			"frame": {
+				"x": 774,
+				"y": 5,
+				"w": 126,
+				"h": 187
+			},
+
+
+			"sourceSize": {
+				"w": 126,
+				"h": 187
+			}
+		},
+
+
+		{
+			"filename": "tile006.png",
+			"frame": {
+				"x": 917,
+				"y": 5,
+				"w": 131,
+				"h": 187
+			},
+			"sourceSize": {
+				"w": 131,
+				"h": 187
+			}
+		},
+
+
+		{
+			"filename": "tile007.png",
+			"frame": {
+				"x": 1063,
+				"y": 11,
+				"w": 134,
+				"h": 182
+			},
+			"sourceSize": {
+				"w": 134,
+				"h": 182
+			}
+		},
+
+
+		{
+			"filename": "tile008.png",
+			"frame": {
+				"x": 1217,
+				"y": 23,
+				"w": 130,
+				"h": 172
+			},
+			"sourceSize": {
+				"w": 130,
+				"h": 172
+			}
+		},
+
+
+		{
+			"filename": "tile009.png",
+			"frame": {
+				"x": 1375,
+				"y": 28,
+				"w": 119,
+				"h": 166
+			},
+			"sourceSize": {
+				"w": 119,
+				"h": 166
+			}
+		},
+
+
+		{
+			"filename": "tile010.png",
+			"frame": {
+				"x": 1526,
+				"y": 17,
+				"w": 119,
+				"h": 175
+			},
+			"sourceSize": {
+				"w": 119,
+				"h": 175
+			}
+		},
+
+
+		{
+			"filename": "tile011.png",
+			"frame": {
+				"x": 1677,
+				"y": 8,
+				"w": 113,
+				"h": 184
+			},
+			"sourceSize": {
+				"w": 113,
+				"h": 184
+			}
+		},
+
+
+		{
+			"filename": "tile012.png",
+			"frame": {
+				"x": 1833,
+				"y": 19,
+				"w": 103,
+				"h": 172
+			},
+			"sourceSize": {
+				"w": 103,
+				"h": 172
+			}
+		},
+
+
+		{
+			"filename": "tile013.png",
+			"frame": {
+				"x": 2000,
+				"y": 32,
+				"w": 80,
+				"h": 160
+			},
+			"sourceSize": {
+				"w": 80,
+				"h": 160
+			}
+		},
+
+
+		{
+			"filename": "tile014.png",
+			"frame": {
+				"x": 2157,
+				"y": 29,
+				"w": 75,
+				"h": 163
+			},
+			"sourceSize": {
+				"w": 75,
+				"h": 163
+			}
+		},
+
+
+		{
+			"filename": "tile015.png",
+			"frame": {
+				"x": 2300,
+				"y": 17,
+				"w": 87,
+				"h": 175
+			},
+			"sourceSize": {
+				"w": 87,
+				"h": 175
+			}
+		},
+
+
+		{
+			"filename": "tile016.png",
+			"frame": {
+				"x": 2443,
+				"y": 9,
+				"w": 109,
+				"h": 184
+			},
+			"sourceSize": {
+				"w": 109,
+				"h": 184
+			}
+		},
+
+
+		{
+			"filename": "tile017.png",
+			"frame": {
+				"x": 2587,
+				"y": 6,
+				"w": 124,
+				"h": 186
+			},
+			"sourceSize": {
+				"w": 124,
+				"h": 186
+			}
+		},
+
+
+		{
+			"filename": "tile018.png",
+			"frame": {
+				"x": 2729,
+				"y": 6,
+				"w": 132,
+				"h": 186
+			},
+			"sourceSize": {
+				"w": 132,
+				"h": 186
+			}
+		},
+
+
+		{
+			"filename": "tile019.png",
+			"frame": {
+				"x": 2875,
+				"y": 11,
+				"w": 134,
+				"h": 183
+			},
+			"sourceSize": {
+				"w": 134,
+				"h": 183
+			}
+		},
+
+
+		{
+			"filename": "tile020.png",
+			"frame": {
+				"x": 3029,
+				"y": 23,
+				"w": 130,
+				"h": 173
+			},
+			"sourceSize": {
+				"w": 130,
+				"h": 173
+			}
+		},
+
+
+		{
+			"filename": "tile021.png",
+			"frame": {
+				"x": 3187,
+				"y": 28,
+				"w": 119,
+				"h": 166
+			},
+			"sourceSize": {
+				"w": 119,
+				"h": 166
+			}
+		},
+
+
+		{
+			"filename": "tile022.png",
+			"frame": {
+				"x": 3342,
+				"y": 17,
+				"w": 115,
+				"h": 175
+			},
+			"sourceSize": {
+				"w": 115,
+				"h": 175
+			}
+		},
+
+
+		{
+			"filename": "tile023.png",
+			"frame": {
+				"x": 3489,
+				"y": 5,
+				"w": 113,
+				"h": 186
+			},
+			"sourceSize": {
+				"w": 113,
+				"h": 186
+			}
+		}
+
+
+	]
+	
+}
