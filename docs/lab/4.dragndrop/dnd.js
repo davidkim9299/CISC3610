691aa085a53fae2a40061aaabd31007338bd25d5
davidkim9299@gmail.com <davidkim9299@gmail.com>
2021-03-30 21:53:51 -0400

    Update dnd.js

--- docs/lab/4.dragndrop/dnd.js
+++ docs/lab/4.dragndrop/dnd.js
@@ -41,18 +41,18 @@ function create(){
     	var eye3 = this.add.sprite(300,100,'eye3').setInteractive();
     	var eye4 = this.add.sprite(400,100,'eye4').setInteractive();
 	var hairline1 = this.add.sprite(100,200,'hairline1').setInteractive();
-    	var hairline2 = this.add.sprite(200,200,'hairline2').setInteractive();
+    	var hairline2 = this.add.sprite(300,200,'hairline2').setInteractive();
 	var lear1 = this.add.sprite(100,300,'lear1').setInteractive();
     	var lear2 = this.add.sprite(200,300,'lear2').setInteractive();
-	var rear1 = this.add.sprite(100,300,'rear1').setInteractive();
-    	var rear2 = this.add.sprite(200,300,'rear2').setInteractive();
-	var mouse1 = this.add.sprite(100,300,'mouse1').setInteractive();
-    	var mouse2 = this.add.sprite(200,300,'mouse2').setInteractive();
-	var mouse3 = this.add.sprite(100,300,'mouse3').setInteractive();
-    	var mouse4 = this.add.sprite(200,300,'mouse4').setInteractive();
-	var nose1 = this.add.sprite(100,300,'nose1').setInteractive();
-    	var nose2 = this.add.sprite(200,300,'nose2').setInteractive();
-	var nose3 = this.add.sprite(100,300,'nose3').setInteractive();
+	var rear1 = this.add.sprite(300,300,'rear1').setInteractive();
+    	var rear2 = this.add.sprite(400,300,'rear2').setInteractive();
+	var mouse1 = this.add.sprite(100,400,'mouse1').setInteractive();
+    	var mouse2 = this.add.sprite(200,400,'mouse2').setInteractive();
+	var mouse3 = this.add.sprite(300,400,'mouse3').setInteractive();
+    	var mouse4 = this.add.sprite(400,400,'mouse4').setInteractive();
+	var nose1 = this.add.sprite(600,100,'nose1').setInteractive();
+    	var nose2 = this.add.sprite(600,200,'nose2').setInteractive();
+	var nose3 = this.add.sprite(600,300,'nose3').setInteractive();
     	var face1 = this.add.sprite(250,500,'face1').setInteractive();
     	var face2 = this.add.sprite(550,500,'face2').setInteractive();
 
@@ -61,6 +61,19 @@ function create(){
     	this.input.setDraggable(eye2);
     	this.input.setDraggable(eye3);
     	this.input.setDraggable(eye4);
+    	this.input.setDraggable(hairline1);
+    	this.input.setDraggable(hairline2);
+    	this.input.setDraggable(lear1);
+    	this.input.setDraggable(lear2);
+    	this.input.setDraggable(rear1);
+    	this.input.setDraggable(rear2);
+    	this.input.setDraggable(mouse1);
+    	this.input.setDraggable(mouse2);
+    	this.input.setDraggable(mouse3);
+    	this.input.setDraggable(mouse4);
+	this.input.setDraggable(nose1);
+    	this.input.setDraggable(nose2);
+    	this.input.setDraggable(nose3);
     	this.input.setDraggable(face1);
     	this.input.setDraggable(face2);
 
