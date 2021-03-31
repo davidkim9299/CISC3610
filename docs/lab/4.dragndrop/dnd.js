c9d3101d5a6f39df36269d9bce5d5bc61c4b4ad0
davidkim9299@gmail.com <davidkim9299@gmail.com>
2021-03-30 21:41:23 -0400

    edit

--- docs/lab/4.dragndrop/dnd.js
+++ docs/lab/4.dragndrop/dnd.js
@@ -1,38 +1,59 @@
-var img_L = 0;
-	var img_T = 0;
-	var targetObj;
+var config = {
+    	type: Phaser.AUTO,
+    	width: 500,
+    	height: 500,
+    	backgroundColor: '#F5F5DC',
+    	parent: 'game',
+    	scene: {
+        	preload: preload,
+        	create: create
+    	}
+};
 
-	function getLeft(o){
-		return parseInt(o.style.left.replace('px', ''));
-	}
-	function getTop(o){
-     		return parseInt(o.style.top.replace('px', ''));
-	}
+var game = new Phaser.Game(config);
 
-	//Move Image
-	function moveDrag(e){
-     		var e_obj = window.event? window.event : e;
-     		var dmvx = parseInt(e_obj.clientX + img_L);
-     		var dmvy = parseInt(e_obj.clientY + img_T);
-     		targetObj.style.left = dmvx +"px";
-     		targetObj.style.top = dmvy +"px";
-     		return false;
-	}
+function preload(){
+    	this.load.image('eye1','images/eye1.png');
+    	this.load.image('eye2','images/eye2.png');
+    	this.load.image('eye3','images/eye3.png');
+    	this.load.image('eye4','images/eye4.png');
+    	this.load.image('face1','images/face1.png');
+    	this.load.image('face2','images/face2.png');
+    	this.load.image('hairline1','images/hairline1.png');
+    	this.load.image('hairline2','images/hairline2.png');
+    	this.load.image('lear1','images/lear1.png');
+    	this.load.image('lear2','images/lear2.png');
+    	this.load.image('mouse1','images/mouse1.png');
+    	this.load.image('mouse2','images/mouse2.png');
+    	this.load.image('mouse3','images/mouse3.png');
+    	this.load.image('mouse4','images/mouse4.png');
+    	this.load.image('nose1','images/nose1.png');
+    	this.load.image('nose2','images/nose2.png');
+    	this.load.image('nose3','images/nose3.png');
+    	this.load.image('rear1','images/rear1.png');
+	this.load.image('rear2','images/rear2.png');
+}
 
-	//Start drag
-	function startDrag(e, obj){
-    		targetObj = obj;
-     		var e_obj = window.event? window.event : e;
-     		img_L = getLeft(obj) - e_obj.clientX;
-     		img_T = getTop(obj) - e_obj.clientY;
+function create(){
+	// position of the sprite
+    	var eye1 = this.add.sprite(100,100,'eye1').setInteractive();
+    	var eye2 = this.add.sprite(200,100,'eye2').setInteractive();
+    	var eye3 = this.add.sprite(300,100,'eye3').setInteractive();
+    	var eye4 = this.add.sprite(400,100,'eye4').setInteractive();
+    	var face1 = this.add.sprite(100,300,'face1').setInteractive();
+    	var face2 = this.add.sprite(200,300,'face2').setInteractive();
 
-     		document.onmousemove = moveDrag;
-     		document.onmouseup = stopDrag;
-     		if(e_obj.preventDefault)e_obj.preventDefault();
-	}
+    	// sprite that you can move around
+    	this.input.setDraggable(eye1);
+    	this.input.setDraggable(eye2);
+    	this.input.setDraggable(eye3);
+    	this.input.setDraggable(eye4);
+    	this.input.setDraggable(face1);
+    	this.input.setDraggable(face2);
 
-	//Stop drag
-	function stopDrag(){
-     		document.onmousemove = null;
-     		document.onmouseup = null;
-	}
+    	this.input.on('drag', function(pointer, gameObject, dragX, dragY){
+        	gameObject.x = dragX;
+        	gameObject.y = dragY;
+    	});
+} 
+
